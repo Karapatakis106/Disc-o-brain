f0eee149933bd51f1f1d1601aa6af583d65d341b
Aggelos Karapatakis <63917743+Karapatakis106@users.noreply.github.com>
2020-10-31 11:37:09 +0200

    Delete CDformat.sh

--- CDformat.sh
+++
@@ -1,5 +0,0 @@
-#! bin/bash
-
-echo "Input your address 1,x,x:"
-    read address
-LXTerminal cdrecord -scanbus && sudo cdrecord blank=fast dev=$address && echo "Process done! " && eject /dev/cd && eject /dev/cdrom && eject /dev/cdrw && sleep 10;
